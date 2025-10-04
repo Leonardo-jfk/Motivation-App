a3b321b8ae292d550b14449e8ff467e41ba5c56c
Leonardo-jfk <soundmagicbusiness@gmail.com>
2025-10-04 15:30:27 -0300

    Initial Commit

---
+++ Motivation/MotivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationApp.swift
+//  Motivation
+//
+//  Created by Leonardo Aurelio on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MotivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotivationTests/MotivationTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationTests.swift
+//  MotivationTests
+//
+//  Created by Leonardo Aurelio on 04/10/2025.
+//
+
+import Testing
+@testable import Motivation
+
+struct MotivationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
