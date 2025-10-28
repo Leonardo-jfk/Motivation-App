6089085b374d355c9804673cdc12a8036b5b8d6f
Leonardo-jfk <soundmagicbusiness@gmail.com>
2025-10-28 20:01:36 -0300

    [clean] the same, trying to get it right

--- Motivation/ContentView.swift
+++ Motivation/ContentView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Leonardo Aurelio on 04/10/2025.
 //
+
 import Foundation
 import SwiftUI
 
@@ -25,7 +26,7 @@ struct ContentView: View {
                 ZStack {
                     RoundedRectangle(cornerRadius: 30, style: .continuous)
                         .fill(Color.black.opacity(0.8))
-                        .frame(width: 200, height: 200)
+                        .frame(width: 100, height: 100)
 
                     Text("Hello, world!")
                         .font(.caption2)
@@ -34,40 +35,43 @@ struct ContentView: View {
                 .padding()
             })
         }
-        .sheet(isPresented: $showingQuote) {
-            quoteDiscription()
-                .presentationDetents([.medium]) // Grande; puedes usar [.medium, .large]
+        if showingQuote == true {
+            show: quoteDiscription()
         }
+                
+        
     }
 }
 
 // Vista de la descripción de la cita
 struct quoteDiscription: View {
     let currectDate: Int = 1
-
+    
     var body: some View {
         let discriptionView = ZStack {
-           
-
-            VStack(spacing: 16) {
+            
+            
+            VStack(spacing: 15) {
                 Text("discribe the world")
                     .font(.title2)
                     .foregroundStyle(.black)
                     .multilineTextAlignment(.center)
-
+                
                 // Ejemplo de contenido adicional
                 Text("Contenido del día \(currectDate)")
                     .font(.subheadline)
                     .foregroundStyle(.black.opacity(0.8))
             }
-           
+            
         }
-        DialogExample(onDismissOutside: true, closeDialog: {
-                            clearFrom()
-                        }, content: discriptionView)
+//        Button("Close"){
+//            onDismiss()
+//        }
+//        DialogExample(onDismissOutside: true, closeDialog: {
+//            clearFrom()
+//        }, content: discriptionView)
     }
 }
-
 #Preview {
-    ContentView()
-}
+        ContentView()
+    }
