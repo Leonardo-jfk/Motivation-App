616b87f8de7a26b92052bb5f757363c7ae2745be
Leonardo-jfk <soundmagicbusiness@gmail.com>
2025-10-28 21:00:55 -0300

    [feature] button grows in size as i wanted

--- Motivation/ContentView.swift
+++ Motivation/ContentView.swift
@@ -23,60 +23,50 @@ struct ContentView: View {
             Button(action: {
                 showingQuote = true
             }, label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30, style: .continuous)
-                        .fill(Color.black.opacity(0.8))
-                        .frame(width: 100, height: 100)
-
-                    Text("Hello, world!")
-                        .font(.caption2)
-                        .foregroundStyle(.white)
-                }
-                .padding()
-            })
-        }.sheet(isPresented: $showingQuote) { quoteDiscription() }
+                if showingQuote == true {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 40, style: .continuous)
+                            .fill(Color.black.opacity(0.8))
+                            .frame(width: 350, height: 350)
+                        
+                        Text("discribe th eworld correctly")
+                            .font(.caption2)
+                            .foregroundStyle(.white)
+                    }
+                    .padding()
+                } else{
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .fill(Color.black.opacity(0.8))
+                            .frame(width: 100, height: 100)
+                        
+                        Text("Hello, world!")
+                            .font(.caption2)
+                            .foregroundStyle(.white)
+                    }
+                    .padding()
+                }})
+                   
+        }//.sheet(isPresented: $showingQuote) { quoteDiscription() }
                 
+//        if showingQuote == true {
+//            ZStack {
+//                RoundedRectangle(cornerRadius: 40, style: .continuous)
+//                    .fill(Color.black.opacity(0.8))
+//                    .frame(width: 300, height: 300)
+//
+//                Text("discribe th eworld correctly")
+//                    .font(.caption2)
+//                    .foregroundStyle(.white)
+//            }
+//            .padding()
+//        }
+    }
         
-    }
+    
 }
 
-// Vista de la descripción de la cita
-struct quoteDiscription: View {
-    var currectDate: Int = 5
-    @Environment(\.dismiss) var dismiss
-//    @Binding var showingQuote:Bool
-    var body: some View {
 
-            ZStack {
-                
-                
-                VStack(spacing: 15) {
-                    Button("come back")
-                    {
-                        dismiss()
-                    }.frame(width: 100, height: 100)
-                        .background(.mainSistem)
-                    
-                    Text("discribe the world")
-                        .font(.title2)
-                        .foregroundStyle(.black)
-                        .multilineTextAlignment(.center)
-                    
-                    // Ejemplo de contenido adicional
-                    Text("Contenido del día \(currectDate)")
-                        .font(.subheadline)
-                        .foregroundStyle(.black.opacity(0.8))
-                }
-            }
-//        }
-//        Button("Close"){
-//            onDismiss()
-//        }
-//        DialogExample(onDismissOutside: true, closeDialog: {
-//            clearFrom()
-//        }, content: discriptionView)
-    }
-}
 #Preview {
         ContentView()
     }
