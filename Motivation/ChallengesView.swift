857528be7ee7f6b51e46478388fa24fdd794012f
Leonardo-jfk <soundmagicbusiness@gmail.com>
2026-01-06 13:55:20 -0300

    [bug] challenge view is ready and it has the button struct, but below there are samurai background

--- Motivation/ChallengesView.swift
+++ Motivation/ChallengesView.swift
@@ -11,19 +11,45 @@ import Lottie
 
 
 public struct ChallengesView: View {
+    
+    
+    var fileName: String = "Girl with books"
+    var contentMode: UIView.ContentMode = .scaleAspectFill
+    var playLoopMode: LottieLoopMode = .loop
+    var onAnimationDidFinish: (() -> Void)? = nil
+    
     public var body: some View {
-        VStack {
-            Text("Challenges List")
-                .font(.largeTitle)
-            Image(systemName: "book").resizable()
-                .scaledToFit()
-                .frame(width: 100, height: 100)
-            // Your future challenges logic here
+        
+        ZStack{
+            LottieView(animation: .named("SciFiBack"))
+                .configure({lottieAnimationView in lottieAnimationView.contentMode = contentMode
+                 lottieAnimationView.animationSpeed = 0.2
+                })
+                
+                .playbackMode(.playing(.toProgress(1, loopMode: playLoopMode)))
+                .animationDidFinish { completed in onAnimationDidFinish?()
+                        
+                }
+                .resizable()
+                .ignoresSafeArea()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            Rectangle()
+                .frame(width:100, height: 200)
+                .foregroundStyle(.white)
+         
+            VStack {
+                Text("Challenges List")
+                    .font(.largeTitle)
+                Image(systemName: "book").resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                // Your future challenges logic here
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
-
 
 #Preview {
     ChallengesView()
