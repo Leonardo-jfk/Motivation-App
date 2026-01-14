5f333f062e4a4ab3d6c1624e3bb7d4613656b7ab
Leonardo-jfk <soundmagicbusiness@gmail.com>
2026-01-14 10:12:31 -0300

    [bug] The `navigationDestination` modifier only works inside a `NavigationStack` or `NavigationSplitView`

--- Motivation/ChallengesView.swift
+++ Motivation/ChallengesView.swift
@@ -12,66 +12,97 @@ import Lottie
 
 public struct ChallengesView: View {
     
+    @Environment(\.colorScheme) var colorScheme
+    @AppStorage("appColorScheme") private var storedScheme: String = AppColorScheme.system.rawValue
+    private var appScheme: AppColorScheme {
+        AppColorScheme(rawValue: storedScheme) ?? .system
+    }
+    
+    
+    
     
     var fileName: String = "Girl with books"
     var contentMode: UIView.ContentMode = .scaleAspectFill
     var playLoopMode: LottieLoopMode = .loop
     var onAnimationDidFinish: (() -> Void)? = nil
     
+//    let BackImage = (colorScheme == .dark) ? "BackDarkMarco" : "BackLightMarco"
+    
     public var body: some View {
+        let backImage = (colorScheme == .dark) ? "BackDarkMarco" : "BackLightMarco"
+//        Image("backImage")
+//            .resizable()
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
         
         ZStack{
-            LottieView(animation: .named("SciFiBack"))
-                .configure({lottieAnimationView in lottieAnimationView.contentMode = contentMode
-                    lottieAnimationView.animationSpeed = 0.2
-                })
-            
-                .playbackMode(.playing(.toProgress(1, loopMode: playLoopMode)))
-                .animationDidFinish { completed in onAnimationDidFinish?()
-                    
-                }
+            Image(backImage)
                 .resizable()
-                .ignoresSafeArea()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
             
             Rectangle()
                 .frame(width:100, height: 200)
                 .foregroundStyle(.white)
-            NavigationStack{
+//            NavigationStack{
                 
                 
                 VStack {
+                    Text("Challenges List")
+                        .font(.largeTitle)
+                    Image(systemName: "book").resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                    
+                    Spacer() 
                     HStack{
                         ZStack{
                             Rectangle()
                                 .frame(width:150, height: 100)
                                 .foregroundStyle(.gray)
                             NavigationLink("1st", destination:
-                                Challenge1View())
-                            }
+                                            Challenge1View())
                         }
-                        Rectangle()
-                            .frame(width:150, height: 100)
-                            .foregroundStyle(.gray)
                         
+                        ZStack{
+                            Rectangle()
+                                .frame(width:150, height: 100)
+                                .foregroundStyle(.gray)
+                            NavigationLink("1st", destination:
+                                            Challenge2View())
+                        }
+                    }
+                    
+                    
+                    HStack{
+                        ZStack{
+                            Rectangle()
+                                .frame(width:150, height: 100)
+                                .foregroundStyle(.gray)
+                            NavigationLink("1st", destination:
+                                            Challenge1View())
+                        }
+                        
+                        ZStack{
+                            Rectangle()
+                                .frame(width:150, height: 100)
+                                .foregroundStyle(.gray)
+                            NavigationLink("1st", destination:
+                                            Challenge2View())
+                        }
                         
                     }
-                    Text("Challenges List")
-                        .font(.largeTitle)
-                    Image(systemName: "book").resizable()
-                        .scaledToFit()
-                        .frame(width: 100, height: 100)
-                    // Your future challenges logic here
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .ignoresSafeArea()
-            }.frame(maxWidth: .infinity)
-                .frame(height: 400)
+                        
+                        // Your future challenges logic here
+                    
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .ignoresSafeArea()
+                }.frame(maxWidth: .infinity)
+                    .frame(height: 400)
+            }
         }
-    }
-
-
-
+//    }
+    
+}
 
 struct Challenge1View: View{
     
@@ -86,6 +117,43 @@ struct Challenge1View: View{
 }
 
 
+struct Challenge2View: View{
+    
+    
+    
+    var body: some View {
+        Image(systemName: "heart")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 50, height: 50, alignment: .center)
+    }
+}
+
+
+struct Challenge3View: View{
+    
+    
+    
+    var body: some View {
+        Image(systemName: "house")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 50, height: 50, alignment: .center)
+    }
+}
+
+
+struct Challenge4View: View{
+    
+    
+    
+    var body: some View {
+        Image(systemName: "house")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 50, height: 50, alignment: .center)
+    }
+}
 
 
 
